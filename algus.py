4e31f6667b6baf65a340de4637245d57cb62efd5
kristiinakulbok <kristiina.kulbok@gmail.com>
2018-11-12 12:06:27 +0200

    ei kommenteeri

--- algus.py
+++ algus.py
@@ -1,19 +1,35 @@
+#from winsound import Beep https://docs.python.org/3.7/library/winsound.html
+
+#iga morse tähemärgi vahel 1 tühik, iga morse sõna vahel 3 tühikut
+
 kasutaja_keel = input("Mis keelest soovid tõlkida (morse, eesti)? ")
-kasutaja_sisend = input("Sisesta sõna, mida soovid tõlkida: ")
+sonastik = {"A":".-", "B":"-...", "C":"-.-.", "D":"-..", "E":".", "F":"..-.", "G":"--.", "H":"....", "I":"..", "J":".---", "K":"-.-", "L":".-..", "M":"--", "N":"-.", "O":"---", "P":".--.", "Q":"--.-", "R":".-.", "S":"...", "T":"-", "U":"..-", "V":"...-", "W":".--", "X":"-..-", "Y":"-.--", "Z":"--.."}
+vastuste_ajalugu = ""
 
-sonastik = {"A":".-", "B":"-...", "C":"-.-.", "D":"-..", "E":".", "F":"..-.", "G":"--.", "H":"....", "I":"..", "J":".---", "K":"-.-"}
-vastus = ""
+while True:
+    kasutaja_sisend = input("Sisesta sõna, mida soovid tõlkida: ")
+    if kasutaja_sisend == "":
+        break
+    vastus = ""
 
-if kasutaja_keel=="eesti":
-    for i in kasutaja_sisend:
-        taht = sonastik[i.upper()]
-        vastus+=taht + " "
+    if kasutaja_keel == "eesti":
+        for i in kasutaja_sisend:
+            if i == " ":
+                vastus += "   "
+            else:
+                taht = sonastik[i.upper()]
+                vastus += taht + " "
 
-if kasutaja_keel=="morse":
-    for el in sonastik:
-        a = kasutaja_sisend.split(" ")
-        for j in a:
-            if sonastik[el]==j:
-                vastus+=el.lower()
+    if kasutaja_keel == "morse":
+        a = kasutaja_sisend.replace("   ", " t ").split(" ") #lihtsustamise mõttes asendan sõnadevahelise tühiku esialgu t-tähega
+        for i in a:
+            if i == "t":
+                vastus += " "
+            else:
+                for j in sonastik:
+                    if sonastik[j] == i:
+                        vastus += j.lower()
 
-print(vastus)
+    vastuste_ajalugu += "Sisend: " + kasutaja_sisend + "; Tõlge: " + vastus + "\n"
+    print(vastus)
+
